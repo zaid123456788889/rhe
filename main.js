44808249a1f48d98f0c6849b1b28addb000c63e4
zaid123456788889 <92422163+zaid123456788889@users.noreply.github.com>
2022-06-20 19:48:26 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,46 @@
+img = "";
+objects = [];
+status = "";
+
+function preload(){
+  img = loadImage('dog_cat.jpg');
+}
+
+function setup() {
+  canvas = createCanvas(640, 420);
+  canvas.center();
+  objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+  document.getElementById("status").innerHTML = "Status : Detecting Objects";
+}
+
+function modelLoaded() {
+  console.log("Model Loaded!")
+  status = true;
+  objectDetector.detect(img, gotResult);
+}
+
+function gotResult(error, results) {
+  if (error) {
+    console.log(error);
+  }
+  console.log(results);
+  objects = results;
+}
+
+function draw() {
+  image(img, 0, 0, 640, 420);
+
+      if(status != "")
+      {
+        for (i = 0; i < objects.length; i++) {
+          document.getElementById("status").innerHTML = "Status:Object Detected";
+    
+          fill("red");
+          percent = floor(objects[i].confidence * 100);
+          text(objects[i].label + "" + percent + "%", objects[i].x + 15, objects[i].y + 15);
+          noFill();
+          stroke("red");
+          rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+        }
+      }
+}
